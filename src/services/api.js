f3d0775a6f69a77c38c7d3b5f79cbf3bbeeb37c1
Alex <alexmoreira64@gmail.com>
2024-03-19 15:03:25 -0300

    fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/api'
+  baseURL: 'https://https://github.com/william-costa/wdev-mock-site-resources/tree/master/api'
 });
 
 export default api;
